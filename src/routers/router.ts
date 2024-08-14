78a5d48e8ce0b45e0de25ab815c5403c3a7f75ad
guilherme bonato <guilhermeferrazbonato@gmail.com>
2024-08-14 11:22:26 -0300

    Rota raiz para verificar se a API está funcionando corretamente

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -4,6 +4,13 @@ import userRoute from './userRoute';
 import { authenticateToken } from '../authMiddleware/authMiddleware';
 
 export default (app: Express) => {
+
+    // Rota raiz para verificar se a API está funcionando corretamente
+    app.get('/', (req, res) => {
+        console.log('Rota raiz acessada'); // Adiciona um console.log para depuração
+        res.send('API funcionando corretamente');
+    });
+
     // Rotas de usuários (registro e login) sem autenticação
     app.use('/api/usuarios', userRoute);
 
