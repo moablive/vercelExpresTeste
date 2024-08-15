2f74e957a92d79edf1aaae4131f33c6899bf19b4
guilherme bonato <guilhermeferrazbonato@gmail.com>
2024-08-15 15:58:40 -0300

    update CORS

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,22 @@ const port = process.env.PORT;
 // Middleware para permitir que o Express interprete JSON no corpo das requisições
 app.use(express.json());
 
-// Middleware para permitir CORS apenas para o seu front-end
+// Middleware para permitir CORS para múltiplas origens (URLs)
+const allowedOrigins = [
+    'https://vercel-vue-teste.vercel.app',
+    'https://vercel-vue-teste-git-main-teammoab.vercel.app',
+    'https://vercel-vue-teste-hf4974775-teammoab.vercel.app',
+    // Adicione outras URLs aqui, se necessário
+];
+
 app.use(cors({
-    origin: 'https://vercel-vue-teste.vercel.app/'
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
 }));
 
 // Verifica a conexão com o banco de dados
